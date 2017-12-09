91a79f574df04d1257755a3eb39007beb4e64ff7
Suryakant Bharti <suryakant.bharti@gmail.com>
2017-12-09 14:29:14 +0530

    adding overriding codes

---
+++ Object-Oriented-Programming/overriding/code1_basic_overriding.java
@@ -0,0 +1,22 @@
+class Animal {
+   public void move() {
+      System.out.println("Animals can move");
+   }
+}
+
+class Dog extends Animal {
+   public void move() {
+      System.out.println("Dogs can walk and run");
+   }
+}
+
+public class TestDog {
+
+   public static void main(String args[]) {
+      Animal a = new Animal();   // Animal reference and object
+      Animal b = new Dog();   // Animal reference but Dog object
+
+      a.move();   // runs the method in Animal class
+      b.move();   // runs the method in Dog class
+   }
+}
