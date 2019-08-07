223c809bddbea77bd28a75f0f4dab2d9b1f835a4
Suryakant Bharti <suryakant.bharti@gmail.com>
2019-08-07 21:01:45 +0530

    adding n/w codes

---
+++ java_io_networking/networking/code2_Socket_Client_to_Server_Msg_ClientCode.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.net.*;
+public class MyClient {
+    public static void main(String[] args) {
+        try{
+            Socket s=new Socket("localhost",6666);
+            DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+            dout.writeUTF("Hello Server");
+            dout.flush();
+            dout.close();
+            s.close();
+        }catch(Exception e){System.out.println(e);}
+    }
+}
