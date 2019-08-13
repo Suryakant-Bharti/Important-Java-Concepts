d6b807684a543fa28941f8bbf8539098c5ef1c98
Suryakant Bharti <suryakant.bharti@gmail.com>
2019-08-13 17:38:07 +0530

    adding n/w codes

---
+++ java_io_networking/networking/code4_Socket_two_way_communication_between_Client_and_Server_ClientCode.java
@@ -0,0 +1,21 @@
+import java.net.*;
+import java.io.*;
+class MyClient{
+    public static void main(String args[])throws Exception{
+        Socket s=new Socket("localhost",3333);
+        DataInputStream din=new DataInputStream(s.getInputStream());
+        DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        String str="",str2="";
+        while(!str.equals("stop")){
+            str=br.readLine();
+            dout.writeUTF(str);
+            dout.flush();
+            str2=din.readUTF();
+            System.out.println("Server says: "+str2);
+        }
+
+        dout.close();
+        s.close();
+    }}
