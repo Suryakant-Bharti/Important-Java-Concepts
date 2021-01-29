7dccd1f5b1ae3f450ec52e5326489f86ff7daf37
Suryakant Bharti <suryakant.bharti@gmail.com>
2021-01-29 21:14:56 +0530

    coding related...

---
+++ java_coding/1DArrays/Array_Partition_I_Better_Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        
+        Arrays.sort(nums);
+        
+        int sum = 0;
+        for(int i = 0; i < nums.length; i+=2)   sum += nums[i];
+        
+        return sum;
+    }
+}
